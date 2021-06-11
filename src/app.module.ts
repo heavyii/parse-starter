2231b06f0ed20857b8ed3af8279d69687065ea3f
heavy <linruisheng@seeklane.com>
2021-06-11 15:31:20 +0800

    添加环境配置文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const debug = require('debug')('data-server:main');
+
+import { ConfigModule, ConfigService } from '@nestjs/config';
+const envFilePath = process.env.NEST_JS_ENV || '.env.local';
 @Module({
-  imports: [],
+  imports: [
+    /**
+     * 环境变量配置文件 NEST_JS_ENV, 默认值'.env.local'
+     */
+    ConfigModule.forRoot({ isGlobal: true, cache: true, envFilePath }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  private readonly logger = new Logger(AppModule.name);
+  constructor(
+    private configService: ConfigService) {
+
+  }
+  onModuleInit() {
+    this.logger.log('load env from ' + envFilePath);
+    debug('APP_ID', this.configService.get('APP_ID'))
+  }
+}
